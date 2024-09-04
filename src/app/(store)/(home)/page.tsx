c5b04c1589d1755c347a64cbc005c3426eba782d
Vinicius S Sanchez <viniciusanchez@gmail.com>
2024-09-04 17:06:07 -0300

    Adicionando produtos na home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main>
-      <p>Hello W </p>
-    </main>
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-never-stop-learning 1.png"
+          className=""
+          alt=""
+          width={860}
+          height={860}
+          quality={100}
+        />
+      </Link>
+      <div></div>
+    </div>
   )
 }
